a22e85c7fc318a002f3a7b2324c8cbfd9f958719
Dejwid <s205915@sggw.edu.pl>
2023-04-02 12:43:59 +0200

    draw_text() and get_sentence()

--- Type_speed_test.py
+++ Type_speed_test.py
@@ -33,6 +33,19 @@ class Game:
         self.screen = pygame.display.set_mode((self.w, self.h))
         pygame.display.set_caption('Type Speed test')
 
+    def draw_text(self, screen, msg, y, fsize, color):
+        font = pygame.font.Font(None, fsize)
+        text = font.render(msg, 1, color)
+        text_rect = text.get_rect(center = (self.w/2, y))
+        screen.blit(text, text_rect)
+        pygame.display.update()
+
+    def get_sentence(self):
+        f = open('images/sentences.txt').read()
+        sentences = f.split('\n')
+        sentence = random.choice(sentences)
+        return sentence   
+
     
 
 
